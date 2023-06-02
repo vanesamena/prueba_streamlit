acad6f1642e0e8f27cee7399f736a360435eb75c
Vanesa Mena <92614813+vanesamena@users.noreply.github.com>
2023-06-02 11:58:26 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Define la interfaz de usuario con Streamlit
+def main():
+
+    st.title("Sistema de Recomendacion de peliculas")
+
+    st.write('Seleccione uno o varios generos')
+    options = ['Terror', 'Drama', 'Aventura', 'Romantica']
+    selected_options = st.multiselect('Selecciona las opciones:', options, max_selections=3)
+
+    options = ['Opción 1', 'Opción 2', 'Opción 3']
+    selected_option = st.selectbox('Selecciona una opción:', options)
+
+    texto_usuario = st.text_input("Texto agregado por el ususario")
